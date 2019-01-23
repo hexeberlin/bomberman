b98e77d01d603f4f8a7078845380db46f7c8acca
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-01-23 15:42:29 +0100

    add explosion animation

--- src/bombs.js
+++ src/bombs.js
@@ -31,9 +31,11 @@ class Bomb {
                     if((Math.abs(row - bRow) <= this.power && col === bCol)
                     || (Math.abs(col - bCol) <= this.power && row === bRow)) {
                         if(myGame.grid[row][col] === 2){
-                            myGame.grid[row][col] = 0;
+                            myGame.grid[row][col] = "E"; //show explosion first
+                            setTimeout(function(){myGame.grid[row][col] = 0;}, 1000);
                         } else if (myGame.grid[row][col] === "D") {
-                            myGame.grid[row][col] = "F" //found the door
+                            myGame.grid[row][col] = "E"; //show explosion first
+                            setTimeout(function(){myGame.grid[row][col] = "F";}, 1000); //found the door
                         }
                     }
                 }
